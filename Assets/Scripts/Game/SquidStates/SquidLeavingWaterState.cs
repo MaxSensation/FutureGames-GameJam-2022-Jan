b18b4ebd38dc6a62774f6c651567b832294ab183
Maximiliam Rosén <karlsson.maximiliam@gmail.com>
2022-01-29 13:03:18 +0100

    Tried to fix the leavingWater mechinic

--- Assets/Scripts/Game/SquidStates/SquidLeavingWaterState.cs
+++ Assets/Scripts/Game/SquidStates/SquidLeavingWaterState.cs
@@ -15,14 +15,17 @@ namespace SquidStates
 
         public void OnEnter()
         {
-            var up = _squidController.transform.up;
-            var rightBoost = Scale( _leavingWaterParams.minimumAngleFromTop, 1f, _leavingWaterParams.minimumBoost, 1f, Vector2.Dot(up, new Vector2(Mathf.Cos(_leavingWaterParams.optimalAngle), Mathf.Sin(_leavingWaterParams.optimalAngle))));
-            var leftBoost = Scale( _leavingWaterParams.minimumAngleFromTop, 1f, _leavingWaterParams.minimumBoost, 1f, Mathf.Abs(Vector2.Dot(up, new Vector2(Mathf.Cos(-_leavingWaterParams.optimalAngle), Mathf.Sin(-_leavingWaterParams.optimalAngle)))));
-            Debug.Log($"{leftBoost} {rightBoost}");
+            var up = (_squidController.Rb.velocity.normalized + (Vector2)_squidController.transform.up).normalized;
+            Debug.Log($"LeftDot: {Vector2.Dot(up, new Vector2(Mathf.Cos(_leavingWaterParams.optimalAngle), Mathf.Sin(_leavingWaterParams.optimalAngle)))} RightDot: {Vector2.Dot(up, new Vector2(Mathf.Cos(-_leavingWaterParams.optimalAngle), Mathf.Sin(-_leavingWaterParams.optimalAngle)))}");
+            var rightBoost = Scale( _leavingWaterParams.minimumAngleFromTop, 1f, _leavingWaterParams.minimumBoost, _leavingWaterParams.force, 
+                Vector2.Dot(up, new Vector2(Mathf.Cos(_leavingWaterParams.optimalAngle), Mathf.Sin(_leavingWaterParams.optimalAngle))));
+            var leftBoost = Scale( _leavingWaterParams.minimumAngleFromTop, 1f, _leavingWaterParams.minimumBoost, _leavingWaterParams.force, 
+                Mathf.Abs(Vector2.Dot(up, new Vector2(Mathf.Cos(-_leavingWaterParams.optimalAngle), Mathf.Sin(-_leavingWaterParams.optimalAngle)))));
+            Debug.Log($"RightBoost: {leftBoost}, LeftBoost: {rightBoost}");
             if (Vector2.Dot(_squidController.transform.up, Vector2.right) > 0)
-                _squidController.Rb.velocity = _squidController.transform.up * _leavingWaterParams.force * rightBoost * _squidController.Rb.velocity.magnitude / _leavingWaterParams.maxVelocityForce;
+                _squidController.Rb.velocity = _squidController.transform.up * (rightBoost * _squidController.Rb.velocity.magnitude / _leavingWaterParams.maxVelocityForce);
             else
-                _squidController.Rb.velocity = _squidController.transform.up * _leavingWaterParams.force * leftBoost * _squidController.Rb.velocity.magnitude / _leavingWaterParams.maxVelocityForce;
+                _squidController.Rb.velocity = _squidController.transform.up * (leftBoost * _squidController.Rb.velocity.magnitude / _leavingWaterParams.maxVelocityForce);
         }
         
         private static float Scale(float oldMin, float oldMax, float newMin, float newMax, float oldValue){
